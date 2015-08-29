e6f435595584eaecc069c82b42762940271715d8
Tyler Cecchi <thcecchi@gmail.com>
2015-08-28 21:55:18 -0400

    initial commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('tipOfTheEisbergRepoApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://<thcecchi>:<tyler91>@ds035503.mongolab.com:35503/hackathon5'
+  },
+
+  seedDB: true
+};
